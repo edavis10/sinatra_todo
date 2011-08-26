83d8885441dccd0691b7d124eb498dd875d23d89
Eric Davis <edavis@littlestreamsoftware.com>
2011-08-25 19:53:17 -0700

    Space out tags

--- lib/todo.rb
+++ lib/todo.rb
@@ -28,7 +28,7 @@ class Todo
     html = ''
     html += link_to_priority(self.priority) + " "
     html += auto_link(self.content) + " "
-    html += tags.inject('') {|tag_content, tag| tag_content += link_to_tag(tag) } + " "
+    html += tags.inject('') {|tag_content, tag| tag_content += link_to_tag(tag) + " " } + " "
     html += edit_link(self)
     html
   end
